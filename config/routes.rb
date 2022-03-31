678a347d93457e429c16f651ded9409eb08b2abb
Leily Coquard <leilycoquard@gmail.com>
2022-03-31 15:47:48 +0200

    login and bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :robots
-  root "pages#home"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "sessions#welcome"
 
-  #Define root path
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
   get '/home' => 'pages#home'
+
 end
