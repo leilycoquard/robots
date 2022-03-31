7c296c7e838097acde10fdae21d4d759a5f7dbed
Leily Coquard <leilycoquard@gmail.com>
2022-03-31 12:02:10 +0200

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :robots
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Define root path
+  get '/home' => 'pages#home'
 end
